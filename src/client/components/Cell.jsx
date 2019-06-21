d7563c85e22485633c556d452aabbd9d15ecdb4a
Adam Jaffe Back <adb33@email.arizona.edu>
2019-06-21 07:47:35 -0700

    (feat) Cell receives click handler prop.

--- src/client/components/Cell.jsx
+++ src/client/components/Cell.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function Cell() {
-	return <div style={{height: '15px', width: '15px', fontSize: '.5px', backgroundColor: 'gray', margin: '-4px .5px', display: 'inline-block'}} />;
+export default function Cell({handleClick}) {
+  const style = {
+    height: '15px',
+    width: '15px',
+    fontSize: '.5px',
+    backgroundColor: 'gray',
+    margin: '-4px .5px',
+    display: 'inline-block',
+  };
+
+	return <div onClick={handleClick} style={style} />;
 }
+
+Cell.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
