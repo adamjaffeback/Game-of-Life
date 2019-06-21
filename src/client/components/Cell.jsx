55153d6524260b3272c92e890c51a257c176da9c
Adam Jaffe Back <adb33@email.arizona.edu>
2019-06-21 08:30:07 -0700

    (feat) When a cell is clicked, toggle living state.

--- src/client/components/Cell.jsx
+++ src/client/components/Cell.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
 export default function Cell({handleCellClick}) {
+  const [living, toggleLiving] = useState(false);
   const style = {
     height: '15px',
     width: '15px',
     fontSize: '.5px',
-    backgroundColor: 'gray',
+    backgroundColor: living ? 'yellow' : 'gray',
     margin: '-4px .5px',
     display: 'inline-block',
   };
 
-	return <div onClick={handleCellClick} style={style} />;
+  const handleClick = ev => {
+    toggleLiving(!living);
+    handleCellClick(ev);
+  };
+
+	return <div onClick={handleClick} style={style} />;
 }
 
 Cell.propTypes = {
