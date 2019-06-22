486c67b4e9e0c3bfbe6163287b37745a841e7232
Adam Jaffe Back <adb33@email.arizona.edu>
2019-06-22 14:18:54 -0700

    (save) Start determining which cells die/live/repro.

--- src/client/lifeHelpers/index.js
+++ src/client/lifeHelpers/index.js
@@ -1,3 +1,31 @@
+import getNeighbors from './neighbors';
+
+function determineChanges(cells) {
+  return cells.map(cell => {
+    // for every cell, count alive neighbors
+    const livingNeighbors = getNeighbors(cell, cells)
+                            .reduce((neighbor, acc) => {
+                              neighbor.living ? acc++ : acc;
+                              return acc;
+                            }, 0);
+
+    // if cell is alive
+    if (cell.living) {
+      // if less than 2 live neighbors (underpop) or more than 3 live neighbors (overpop)
+      if (livingNeighbors < 2 || livingNeighbors > 3) {
+        cell.shouldToggle = true;
+      }
+    // else cell is dead and has 3 live neighbors (repro)
+    } else if (!cell.living) {
+      if (livingNeighbors === 3) {
+        cell.shouldToggle = true;
+      }
+    }
+  });
+}
+
 export default function generate (cells = []) {
-  return cells;
+  // evaluate dial to determine who lives, dies, and reproduces
+  return determineChanges(cells);
+  // TODO: make those changes
 }
---
+++ src/client/lifeHelpers/neighbors.js
@@ -0,0 +1,26 @@
+export function isNeighborCell(home, potentialNeighbor) {
+  if (home.identity === neighbor.identity) {
+    return false;
+  } else {
+    const hLoc = home.location;
+    const nLoc = potentialNeighbor.location;
+
+    return (nLoc.x >= hLoc.x - 16 && nLoc.x <= hLoc.x + 16) &&
+           (nLoc.y >= hLoc.y - 18 && nLoc.y <= hLoc.y + 18);
+  }
+}
+
+export default function getNeighbors(home, cells) {
+  const CELL_COUNT = cells.length;
+  const neighbors = [];
+
+  // break once you find all the neighbors
+  for (let i = 0; i < CELL_COUNT && neighbors.length < 8; i++) {
+    let potentialNeighbor = cells[i];
+    if (isNeighborCell(home, potentialNeighbor)) {
+      neighbors.push(potentialNeighbor);
+    }
+  }
+
+  return neighbors;
+};
