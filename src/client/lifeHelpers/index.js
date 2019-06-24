75e289aef9288502c9e3773c474ece7668b31639
Adam Jaffe Back <adb33@email.arizona.edu>
2019-06-23 22:20:47 -0700

    (refactor) Significantly improve performance by targeting a limited numbers of cells which could possible update.

--- src/client/lifeHelpers/index.js
+++ src/client/lifeHelpers/index.js
@@ -1,28 +1,61 @@
 import getNeighbors from './neighbors';
 
-function determineChanges(cells) {
+function determineChanges (cells) {
+  const livingCellSet = evaluateLivingCells(cells);
+  const deadCellsSet = evaluateDeadCells(cells);
   return cells.map(cell => {
-    // for every cell, count alive neighbors
+    const identity = cell.identity;
+
+    if (livingCellSet.has(identity) || deadCellsSet.has(identity)) {
+      cell.shouldToggle = true;
+    }
+
+    return cell;
+  });
+}
+
+function evaluateLivingCells (cells) {
+  return new Set(cells.filter(cell => cell.living)
+  .map(cell => {
     const livingNeighbors = getNeighbors(cell, cells)
                             .reduce((acc, neighbor) => {
                               return neighbor.living ? acc + 1 : acc;
                             }, 0);
 
-    // if cell is alive
-    if (cell.living) {
-      // if less than 2 live neighbors (underpop) or more than 3 live neighbors (overpop)
-      if (livingNeighbors < 2 || livingNeighbors > 3) {
-        cell.shouldToggle = true;
-      }
-    // else cell is dead and has 3 live neighbors (repro)
-    } else if (!cell.living) {
-      if (livingNeighbors === 3) {
-        cell.shouldToggle = true;
-      }
+    // if less than 2 live neighbors (underpop) or more than 3 live neighbors (overpop)
+    if (livingNeighbors < 2 || livingNeighbors > 3) {
+      cell.shouldToggle = true;
     }
 
     return cell;
-  });
+  })
+  .filter(cell => cell.shouldToggle)
+  .map(cell => cell.identity));
+}
+
+function evaluateDeadCells (cells) {
+  // get living cells
+  return new Set(cells.filter(cell => cell.living)
+  // get the living cells' neighbors
+  .reduce((acc, cell) => acc.concat(getNeighbors(cell, cells)), [])
+  // get only the dead neighbors
+  .filter(cell => !cell.living)
+  // for every dead neighbor
+  .map(cell => {
+    const livingNeighbors = getNeighbors(cell, cells)
+                            .reduce((acc, neighbor) => {
+                              return neighbor.living ? acc + 1 : acc;
+                            }, 0);
+
+    // has 3 live neighbors (repro)
+    if (livingNeighbors === 3) {
+      cell.shouldToggle = true;
+    }
+
+    return cell;
+  })
+  .filter(cell => cell.shouldToggle)
+  .map(cell => cell.identity));
 }
 
 export default function generate (cells = []) {
